6d87b1067c0f1730b4947efa4f3ecf08541cf2f9
Anis El Rabaa <ase29@mail.aub.edu>
2024-11-06 12:51:38 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+onload = function() {
+
+    FloatingAlert.init();
+
+    this.document.getElementById('btn-try-me').addEventListener('click', function(e) {
+        e.preventDefault();
+        FloatingAlert.display('This is a sample alert!');
+    });
+
+
+    // the form
+    this.document.getElementById('try-it-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        FloatingAlert.display(this.alert_text.value);
+    })
+}
+
+
